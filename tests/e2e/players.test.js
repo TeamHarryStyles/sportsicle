d93ebcc70bd991f09a981e798b5a12078433ad71
Joe Churchman <churchman.joe@gmail.com>
2017-08-07 18:07:42 -0700

    player get all from db passes test

--- tests/e2e/players.test.js
+++ tests/e2e/players.test.js
@@ -1,15 +1,22 @@
-const db = require('./helpers/db');
 const request = require('./helpers/request');
 const { assert } = require('chai');
 require('../../lib/connect');
 
 describe('players REST api', () => {
 
+    let player = null;
+
     it('GETs all players from the databse', () => {
         return request.get('/api/players')
             .then(res => res.body)
             .then(players => {
+                player = players[40];
                 assert.ok(players.length > 0);
             });
     });
+
+    it('GETs a player from the API via ID', () => {
+        
+    })
 });
+
