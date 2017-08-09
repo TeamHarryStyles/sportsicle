07858dc0e523590c5fe35327931981bf6aad7ee7
Pierre Salumu-Shabani <jrsalshab@outlook.com>
2017-08-08 18:08:45 -0700

    the league and the team models and tests are being built not yet finished

--- tests/unit/league.test.js
+++ tests/unit/league.test.js
@@ -4,15 +4,14 @@ const User = require('../../lib/models/user');
 const tokenService = require('../../lib/auth/token-service');
 
 describe('league model', () => {
-    it('validates with required fields', () => {
-        const user1 = new User({
-            email: 'tsttxt@woo.com',
-            password: 'abc',
-
-
+    it.only('validates with required fields', () => {
+        const league = new League({
+            users: [{
+                user: new User(),
+                score: new User()
+            }]
         });
-        const password = 'abc';
-        user1.generateHash(password);
+        return league.validate();
     });
 });
 
---
+++ tests/unit/team.test.js
@@ -0,0 +1,7 @@
+// const Team = require('../../lib/models/team');
+// const Player = require('../../lib/models/player');
+// const { assert } = require('chai');
+
+// describe('Team model', () => {
+
+// });
