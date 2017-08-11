078029cf30da40e04b5c922cc559549894f24b2b
Joe Churchman <churchman.joe@gmail.com>
2017-08-11 12:06:49 -0700

    resolved server response issue w/db connect

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 const app = require('./lib/app'); //TODO: initialize app file
 
-require('./lib/connect'); //DONE: write connect file
-
+const connect = require('./lib/connect'); //DONE: write connect file
+connect(process.env.MONGODB_URI);
 const server = http.createServer(app);
 
 const port = process.env.PORT || 3000;
